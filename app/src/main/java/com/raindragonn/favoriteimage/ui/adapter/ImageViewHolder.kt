844653a834332acd7a06f5531cb931349bb5d587
raindragonn <raindragonn@gmail.com>
2024-01-28 00:30:06 +0900

    feat: placeholder 추가

--- app/src/main/java/com/raindragonn/favoriteimage/ui/adapter/ImageViewHolder.kt
+++ app/src/main/java/com/raindragonn/favoriteimage/ui/adapter/ImageViewHolder.kt
@@ -4,6 +4,7 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.raindragonn.favoriteimage.R
 import com.raindragonn.favoriteimage.databinding.ItemImageBinding
 import com.raindragonn.favoriteimage.domain.entity.Image
 
@@ -19,6 +20,7 @@ class ImageViewHolder(
 
         requestManager
             .load(image.originUrl)
+            .placeholder(R.drawable.shape_image_place_holder)
             .thumbnail(thumbNailRequest)
             .centerCrop()
             .transition(DrawableTransitionOptions.withCrossFade())
